7b8bfee2fb70fa45867486ea82af502acd21e908
covaruber <cova_ruber@live.com.mx>
2022-12-04 18:45:24 -0600

    Initial commit

---
+++ R/replaceValues.R
@@ -0,0 +1,13 @@
+replaceValues <- function(Source, Search, Replace){
+  if (length(Search) != length(Replace))
+    stop("Search and Replace Must Have Equal Number of Items\n")
+  Changed <- as.character(Source)
+  for (i in 1:length(Search)) {
+    Changed <- replace(Changed, Changed == Search[i], Replace[i])
+  }
+  
+  if(is.numeric(Replace))
+    Changed <- as.numeric(Changed)
+  
+  return(Changed)
+}
