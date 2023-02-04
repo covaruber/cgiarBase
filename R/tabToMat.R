9f3b7a901614d8c8e2ff022ac5433edfba8db160
covaruber <cova_ruber@live.com.mx>
2023-02-04 17:53:15 +0800

    p commit

--- R/tabToMat.R
+++ R/tabToMat.R
@@ -1,5 +1,5 @@
 tabToMat <- function(Adf){
-  A <- matrix(NA, nrow=max(Adf$Var1n), ncol = max(Adf$Var2n))
+  A <- matrix(NA, nrow=max(Adf$Var1n,na.rm = TRUE), ncol = max(Adf$Var2n, na.rm = TRUE))
   A[as.matrix(Adf[,c("Var1n","Var2n")])] = Adf[,"Freq"]
   A[lower.tri(A)] <- t(A)[lower.tri(A)] # fill the lower triangular
   rownames(A) <- colnames(A) <- levels(Adf$Var1)
