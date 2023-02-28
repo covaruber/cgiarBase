6b89307990b14bc616c80c62a4b3e1996cbef19c
covaruber <cova_ruber@live.com.mx>
2023-02-28 09:33:18 +0800

    p commit

--- R/cleanChar.R
+++ R/cleanChar.R
@@ -1,16 +1,16 @@
 cleanChar <- function(x){
 
-  x <- gsub("dummy_pipeline_","",x)
-  x <- gsub("dummy_season_","",x)
-  x <- gsub("dummy_location_","",x)
-  x <- gsub("dummy_country_","",x)
-  x <- gsub("dummy_trial_","",x)
+  x <- gsub("base_pipeline_","",x)
+  x <- gsub("base_season_","",x)
+  x <- gsub("base_location_","",x)
+  x <- gsub("base_country_","",x)
+  x <- gsub("base_trial_","",x)
 
-  x <- gsub("dummy_pipeline","",x)
-  x <- gsub("dummy_season","",x)
-  x <- gsub("dummy_location","",x)
-  x <- gsub("dummy_country","",x)
-  x <- gsub("dummy_trial",".",x)
+  x <- gsub("base_pipeline","",x)
+  x <- gsub("base_season","",x)
+  x <- gsub("base_location","",x)
+  x <- gsub("base_country","",x)
+  x <- gsub("base_trial",".",x)
 
   x <- gsub("[[:punct:]]", "_", x)
   x <- gsub("___","_",x)
