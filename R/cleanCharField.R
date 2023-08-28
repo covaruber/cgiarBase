b55925ab6f959d70d94aa10806c1a2d99781c8c3
covaruber <cova_ruber@live.com.mx>
2023-08-28 07:21:56 +0300

    clean fields

---
+++ R/cleanCharField.R
@@ -0,0 +1,25 @@
+cleanCharField <- function(x){
+  
+  x <- gsub("base_pipeline_","",x)
+  x <- gsub("base_season_","",x)
+  x <- gsub("base_location_","",x)
+  x <- gsub("base_country_","",x)
+  x <- gsub("base_trial_","",x)
+  
+  x <- gsub("base_pipeline","",x)
+  x <- gsub("base_season","",x)
+  x <- gsub("base_location","",x)
+  x <- gsub("base_country","",x)
+  x <- gsub("base_trial",".",x)
+  
+  x <- toupper(x) # everything goes to upper case
+  x <- gsub(" ", "-", x) # for fieldinstances white spaces become dashes
+  x <- gsub("[[:punct:]]", "-", x) # all special characters become "-"
+  x <- gsub("-----","-",x) # ensure a single "-"
+  x <- gsub("----","-",x) # ensure a single "-"
+  x <- gsub("---","-",x) # ensure a single "-"
+  x <- gsub("--","-",x) # ensure a single "-"
+  x <- gsub("\xd1","N",x)
+  
+  return(x)
+}
